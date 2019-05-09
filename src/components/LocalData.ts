a3814718b7bdb38eb805033cce4ff22d6854325f
Miklos Danka <danka.miklos@gmail.com>
2019-05-09 19:25:31 +0200

    Updated LocalData to use IndexedDB

--- src/components/LocalData.ts
+++ src/components/LocalData.ts
@@ -1,5 +1,15 @@
-export interface ILocalData {
-    version: 1;
+import { openDB, IDBPDatabase, DBSchema } from "idb";
+
+const OBJECT_STORE_NAME = "CZECH_LOCAL_DATA";
+
+interface CzechDb extends DBSchema {
+    [OBJECT_STORE_NAME]: {
+        key: string;
+        value: ILocalDataV1;
+    };
+}
+
+export interface ILocalDataV1 {
     scores: IScores;
     settings: {
         selectedCases: number[];
@@ -13,10 +23,7 @@ export interface IScores {
 }
 
 export class LocalData {
-    private static KEY = "CZECH_APP_LOCAL_DATA";
-    private static CURRENT_VERSION = 1 as 1;
-    private static DEFAULT_LOCAL_DATA: ILocalData = {
-        version: LocalData.CURRENT_VERSION,
+    public static DEFAULT_LOCAL_DATA: ILocalDataV1 = {
         scores: {
             correct: 0,
             wrong: 0,
@@ -26,49 +33,65 @@ export class LocalData {
             selectedCases: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
         },
     };
-    private localData: ILocalData;
+
+    private static DB_NAME = "CZECH_APP_DB";
+    private static KEY = "CZECH_APP_LOCAL_DATA";
+    private static CURRENT_VERSION = 1 as 1;
+
+    private dbPromise: Promise<IDBPDatabase<CzechDb>> | undefined;
+    private localDataPromise: Promise<ILocalDataV1>;
 
     public constructor() {
-        this.localData = this.loadLocalData();
+        if (!("indexedDB" in window)) {
+            console.error("This browser doesn't support IndexedDB, so not storing data");
+            this.localDataPromise = Promise.resolve(LocalData.DEFAULT_LOCAL_DATA);
+            return;
+        }
+        this.localDataPromise = this.loadLocalData();
     }
 
     public getLocalData = () => {
-        return this.localData;
+        return this.localDataPromise;
     };
 
-    public setSelectedCases = (selectedCases: Set<number>) => {
-        this.localData = {
-            ...this.localData,
-            settings: { ...this.localData.settings, selectedCases: Array.from(selectedCases.values()) },
+    public setSelectedCases = async (selectedCases: Set<number>) => {
+        const localData = await this.localDataPromise;
+        const newLocalData = {
+            ...localData,
+            settings: { ...localData.settings, selectedCases: Array.from(selectedCases.values()) },
         };
-        this.saveLocalData();
+        this.localDataPromise = Promise.resolve(newLocalData);
+        return this.saveLocalData();
     };
 
-    public setScores = (scores: IScores) => {
-        this.localData = {
-            ...this.localData,
+    public setScores = async (scores: IScores) => {
+        const localData = await this.localDataPromise;
+        const newLocalData = {
+            ...localData,
             scores,
         };
-        this.saveLocalData();
+        this.localDataPromise = Promise.resolve(newLocalData);
+        return this.saveLocalData();
     };
 
-    private saveLocalData = () => {
-        window.localStorage.setItem(LocalData.KEY, JSON.stringify(this.localData));
-    };
-
-    private loadLocalData = (): ILocalData => {
-        const localDataString = window.localStorage.getItem(LocalData.KEY);
-        if (localDataString == null) {
-            return LocalData.DEFAULT_LOCAL_DATA;
+    private saveLocalData = async () => {
+        window.localStorage.setItem(LocalData.KEY, JSON.stringify(this.localDataPromise));
+        if (this.dbPromise === undefined) {
+            return;
         }
-        const localDataJson = JSON.parse(localDataString);
-        if (this.isLocalData(localDataJson)) {
-            return localDataJson;
-        }
-        return LocalData.DEFAULT_LOCAL_DATA;
+        const db = await this.dbPromise;
+        const localData = await this.localDataPromise;
+        return db.put(OBJECT_STORE_NAME, localData, LocalData.KEY);
     };
 
-    private isLocalData(json: any): json is ILocalData {
-        return json && json.version === LocalData.CURRENT_VERSION;
-    }
+    private loadLocalData = async (): Promise<ILocalDataV1> => {
+        this.dbPromise = openDB<CzechDb>(LocalData.DB_NAME, LocalData.CURRENT_VERSION, {
+            upgrade: db => {
+                db.createObjectStore(OBJECT_STORE_NAME);
+            },
+        });
+        const db = await this.dbPromise;
+        const localData = await db.get(OBJECT_STORE_NAME, LocalData.KEY);
+        return localData === undefined ? LocalData.DEFAULT_LOCAL_DATA : localData;
+    };
 }
