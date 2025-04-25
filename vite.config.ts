bed968054be6cf45d2cf6fa559d8430a0d93bf05
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2025-04-25 22:46:34 +0100

    New icons

--- vite.config.ts
+++ vite.config.ts
@@ -14,23 +14,63 @@ export default defineConfig({
         name: "Czech Practice",
         short_name: "Czech",
         description: "Practise Czech grammar and declensions in an interactive app with over 50000 words",
-        background_color: '#ffffff',
+        background_color: "#ffffff",
         theme_color: "#222222",
         start_url: "/?utm_source=a2hs",
         display: "standalone",
         icons: [
           {
-            src: "/favicon_32x32.png",
-            sizes: "32x32",
-            type: "image/png"
+            "src": "icons/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
           },
           {
-            src: "/favicon_512x512.png",
-            sizes: "512x512",
-            type: "image/png"
+            "src": "icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           }
-        ]
-      },
+        ],
+      }
     })
   ],
   // Optional: Configure server port if needed (default is 5173)
