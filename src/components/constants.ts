35894170ca78377969e49bf7fb5f944576c977cc
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2026-01-04 19:35:21 +0100

    Refactor functions, split into to screens

---
+++ src/components/constants.ts
@@ -0,0 +1,38 @@
+export const ALL_CASE_NAMES = [
+    "", // to index cases from 1
+    "Nominative (1.) - singular",
+    "Genitive (2.) - singular",
+    "Dative (3.) - singular",
+    "Accusative (4.) - singular",
+    "Vocative (5.) - singular",
+    "Locative (6.) - singular",
+    "Instrumental (7.) - singular",
+    "Nominative (1.) - plural",
+    "Genitive (2.) - plural",
+    "Dative (3.) - plural",
+    "Accusative (4.) - plural",
+    "Vocative (5.) - plural",
+    "Locative (6.) - plural",
+    "Instrumental (7.) - plural",
+];
+
+// Setting the order, skipping nominative singular
+export const SELECTABLE_CASE_NUMBERS = [8, 2, 9, 3, 10, 4, 11, 5, 12, 6, 13, 7, 14];
+
+export const CASE_PREPOSITIONS = [
+    "",
+    "to je",
+    "bez",
+    "k/ke",
+    "vidím",
+    "ahoj",
+    "o",
+    "s/se",
+    "to jsou",
+    "bez",
+    "k/ke",
+    "vidím",
+    "ahoj",
+    "o",
+    "s/se",
+];
